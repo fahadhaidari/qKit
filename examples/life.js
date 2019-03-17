f4d0448c95d71a4cbc3736900681b794af3638f9
ADSumADS <45086773+ADSumADS@users.noreply.github.com>
2019-03-17 11:38:07 -0700

    response to review by Fahad Haidari

--- examples/life.js
+++ examples/life.js
@@ -1,8 +1,8 @@
 qKit.init({border:{width:8,color:'#824222'}});
 
-const suffusion	=192;
+const suffusion  =192;
 
-var humor=new Array(2);
+const humor=new Array(2);
 humor[0]=new Array(9);
 humor[1]=new Array(9);
 
@@ -28,92 +28,122 @@ humor[1][6]='#38302C';
 humor[1][7]='#302820';
 humor[1][8]='#281818';
 
-var habitat=new Array(2);
+const habitat=new Array(2);
 habitat[0]=new Array(suffusion);
 habitat[1]=new Array(suffusion);
-for(var r=0;r<suffusion;r++)
+qKit.repeat(r =>
 {
-	habitat[0][r]=new Array(suffusion);
-	habitat[1][r]=new Array(suffusion);
-}
+  habitat[0][r]=new Array(suffusion);
+  habitat[1][r]=new Array(suffusion);
+}, suffusion);
 
 var phase=0;
 var auto=true;
 var scroll=true;
 
-function Random5050Habitat()
+const random5050Habitat = function()
 {
-	phase=0;
+  phase=0;
 
-	for(var r=0;r<suffusion;r++)for(var c=0;c<suffusion;c++)
-	{
-		habitat[0][r][c]=(Math.floor(Math.random()*2)<<2)-1;
-		habitat[1][r][c]=(Math.floor(Math.random()*2)<<2)-1;
-	}
+  qKit.repeat(r =>
+  {
+    qKit.repeat(c =>
+    {
+      habitat[0][r][c]=(qKit.util.randomInt(0, 2)*2)-1;
+      habitat[1][r][c]=(qKit.util.randomInt(0, 2)*2)-1;
+    }, suffusion);
+  }, suffusion);
 }
 
-function PopulateOneWalker()
+const populateOneWalker = function()
 {
-	var m=Math.floor(suffusion/2);//middle
+  const m=Math.floor(suffusion/2);//middle
 
-	for(var r=0;r<suffusion;r++)for(var c=0;c<suffusion;c++)habitat[0][r][c]=habitat[1][r][c]=-1;
+  qKit.repeat(r =>
+  {
+    qKit.repeat(c =>
+    {
+      habitat[0][r][c]=habitat[1][r][c]=-1;
+    }, suffusion);
+  }, suffusion);
 
-	//establish prior phase history
-	habitat[0][m-1][m-1]=3;
-	habitat[0][m-1][m  ]=3;
-	habitat[0][m-1][m+1]=3;
-	habitat[0][m  ][m+1]=3;
-	habitat[0][m+1][m  ]=3;
+  //establish prior phase history
+  habitat[0][m-1][m-1]=3;
+  habitat[0][m-1][m  ]=3;
+  habitat[0][m-1][m+1]=3;
+  habitat[0][m  ][m+1]=3;
+  habitat[0][m+1][m  ]=3;
 
-	//evolve to display-stage
-	phase=0;
-	ComputeNextPhase();
-	phase=1;
+  //evolve to display-stage
+  phase=0;
+  ComputeNextPhase();
+  phase=1;
 }
 
-function ComputeNextPhase()
+const computeNextPhase = function()
 {
-	for(var r=0;r<suffusion;r++)for(var c=0;c<suffusion;c++)
-	{
-		//compute accumulator as positive=alive, negatve=dead; magntitude = count of living neighbors
-		var n=0;for(var j=-1;j<=1;j++)for(var i=-1;i<=1;i++)if((i!=0)||(j!=0))
-			n+=(Math.sign(habitat[phase%2][(suffusion+r+j)%suffusion][(suffusion+c+i)%suffusion])+1)>>1;
+  qKit.repeat(r =>
+  {
+    qKit.repeat(c =>
+    {
+      //compute accumulator as positive=alive, negative=dead; magntitude = count of living neighbors
+      let n=0;
+      for(let j=-1;j<=1;j++)
+        for(let i=-1;i<=1;i++)
+          if((i!=0)||(j!=0))
+            n+=(Math.sign(habitat[phase%2][(suffusion+r+j)%suffusion][(suffusion+c+i)%suffusion])+1)>>1;
 
-		//3 living neighbors yield life in next phase, if alive and 2 neighbors then remains alive
-		//positive=alive, negatve=dead; neighbor-count recorded as magnitude for color-coding ("humor")
-		habitat[(phase+1)%2][r][c]=(n==3 || (n==2 && Math.sign(habitat[phase%2][r][c])==1))?n+1:-(n+1);
-	}
+      //3 living neighbors yield life in next phase, if alive and 2 neighbors then remains alive
+      //positive=alive, negatve=dead; neighbor-count recorded as magnitude for color-coding ("humor")
+      habitat[(phase+1)%2][r][c]=(n==3 || (n==2 && Math.sign(habitat[phase%2][r][c])==1))?n+1:-(n+1);
+    }, suffusion);
+  }, suffusion);
 }
 
-const cellSize	=qKit.screen.width/suffusion;
-for(var r=0;r<suffusion;r++)for(var c=0;c<suffusion;c++)qKit.draw.quad
-({
-	x:c*(	cellSize),y:r*(	cellSize),
-	width:	cellSize,height:cellSize,
+const cellSize = qKit.screen.width / suffusion;
 
-	color:	0,
+qKit.repeat(r =>
+{
+  qKit.repeat(c =>
+  {
+    qKit.draw.quad
+    ({
+      x:c*(  cellSize),y:r*(  cellSize),
+      width:  cellSize,height:cellSize,
 
-	group: 'matrix',
-	extension:{id:`${r}-${c}`,}
+      color:  '#000000',
+
+      group: 'matrix'
+    });
+  }, suffusion);
+}, suffusion);
+
+const matrix = qKit.groups('matrix');
+
+const renderCurrentPhase = function()
+{
+  qKit.repeat(i =>
+  {
+    //offset diagonally for each phase if scrolling is on
+    const x=((i%suffusion)+suffusion+(scroll?phase:0))%suffusion;
+    const y=(Math.floor(i/suffusion)+suffusion+(scroll?phase:0))%suffusion;
+
+    //color-code by living/dead and prior neighbor-count
+    matrix[i].color=humor[(Math.sign(habitat[(phase+1)%2][y][x])+1)>>1][Math.abs(habitat[phase%2][y][x])-1];
+  }, matrix.length);
+}
+
+random5050Habitat();
+renderCurrentPhase();
+
+qKit.update(() => 
+{
+  if(auto)
+  {
+    computeNextPhase();
+
+    phase=(phase+1)%suffusion;
+
+    renderCurrentPhase();
+  }
 });
-
-const matrix=qKit.groups('matrix');
-
-function RenderCurrentPhase()
-{
-	for(var i=0;i<matrix.length;i++)
-	{
-		//offset diagonally for each phase if scrolling is on
-		var x=((i%suffusion)+suffusion+(scroll?phase:0))%suffusion;
-		var y=(Math.floor(i/suffusion)+suffusion+(scroll?phase:0))%suffusion;
-
-		//color-code by living/dead and prior neighbor-count
-		matrix[i].color=humor[(Math.sign(habitat[(phase+1)%2][y][x])+1)>>1][Math.abs(habitat[phase%2][y][x])-1];
-	}
-}
-
-Random5050Habitat();
-//PopulateOneWalker();
-RenderCurrentPhase();
-
-qKit.update(() => {if(auto){ComputeNextPhase();phase=(phase+1)%suffusion;RenderCurrentPhase();}});
